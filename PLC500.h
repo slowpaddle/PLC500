817b7c2e4ab67b8229b9238580102d0557904487
slowpaddle <slowpaddle@gmail.com>
2023-04-02 14:29:39 -0400

    Update PLC500.h

--- PLC500.h
+++ PLC500.h
@@ -8,17 +8,18 @@
 
 class PLC500 {
   public:
-  void begin();
-  void setY(int x, bool y);
-  void setYA(int x, int y);
-  bool readX(int x);
-  bool readY(int x);
-  int readXA(int x);
-  int readYA(int x);
-  int inX[17];
-  int outY[13];
-  int inXA[17];
-  int outYA[9]; 
+    void begin();
+    void setY(int x, bool y);
+    void setYA(int x, int y);
+    bool readX(int x);
+    bool readY(int x);
+    int readXA(int x);
+    int readYA(int x);
+  private:
+    int inX[17];
+    int outY[13];
+    int inXA[17];
+    int outYA[9]; 
 };
 //extern _GetCSV GetCSV;
 
